23352a9421361c7181ab65b3f58079e971ac581b
Zick_Zibn <karpenko_ilia@list.ru>
2026-03-06 10:52:19 +0300

    fix commit

--- src/main/java/ru/zickzibn/springboot/service/UserService.java
+++ src/main/java/ru/zickzibn/springboot/service/UserService.java
@@ -1,35 +1,16 @@
 package ru.zickzibn.springboot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 import ru.zickzibn.springboot.entity.User;
-import ru.zickzibn.springboot.repository.UserRepository;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
+public interface UserService {
+    List<User> findAll();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    void save(User user);
 
-    }
+    void deleteById(Long id);
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public void save(User user) {
-        userRepository.save(user);
-    }
-
-    public void deleteById(Long id) {
-        userRepository.deleteById(id);
-    }
-
-    public User findUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
+    User findUserById(Long id);
 }
---
+++ src/main/java/ru/zickzibn/springboot/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package ru.zickzibn.springboot.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.zickzibn.springboot.entity.User;
+import ru.zickzibn.springboot.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    @Override
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
